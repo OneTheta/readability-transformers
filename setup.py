63f713173cf8cf048089fd523896a95a042bd964
Chan Woo Kim <chanwkim01@gmail.com>
2021-07-05 05:44:23 +0000

    setup dist

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+print(find_packages())
+setup(
+  name = 'readability-transformers',      
+  packages=find_packages(),
+  version = '0.1.1',     
+  license='Apache License 2.0',       
+  description = 'Package for integrating transformer modules for readability-related NLP tasks.',  
+  long_description=open("README.md", "r", encoding="utf-8").read(),
+  long_description_content_type="text/markdown",
+  author = 'Chan Woo Kim',                 
+  author_email = 'chanwkim01@gmail.com',    
+  url = 'https://github.com/OneTheta/readability-transformers', 
+  download_url = 'https://github.com/OneTheta/readability-transformers/archive/refs/tags/0.0.1.tar.gz',    
+  python_requires=">=3.6.0",
+  keywords = ['transformers'],   
+  install_requires=[          
+    'loguru',
+    'numpy',
+    'pandas',
+    'easydict',
+    'spacy',
+    'torch',
+    'tqdm',
+    'sentence-transformers',
+    'TRUNAJOD'
+],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    "License :: OSI Approved :: Apache Software License",
+    'Programming Language :: Python :: 3.6',
+  ],
+)

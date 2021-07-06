5a139ade7affcb08d411a72e23d9eaf8be2d98e2
Chan Woo Kim <chanwkim01@gmail.com>
2021-07-06 11:26:36 +0000

    install requires for extra feeatures

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     'TRUNAJOD'
     'requests'
     'gensim'
-    'supar==1.1.1',
+    'supar',
 ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
